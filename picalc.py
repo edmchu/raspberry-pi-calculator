23160f83842b35e48d206fc43e943077ee364b5c
edmchu <eemcgough@icloud.com>
2026-04-08 21:26:32 -0500

    Create picalc.py

---
+++ picalc.py
@@ -0,0 +1,9 @@
+def calculate (intoperand, num1, num2) :
+  if intoperand == "+" :
+    return(num1+num2)
+  elif intoperand == "-" :
+    return(num1-num2)
+  elif intoperand == "*" :
+    return(num1*num2)
+  elif intoperand == "/" :
+    return(num1/num2)
